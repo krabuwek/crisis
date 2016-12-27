22b7b5767d194d669e4f32673141b9e404af6888
Max <krabuwek@outlook.com>
2016-12-28 02:41:37 +0300

    init

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -272,7 +272,10 @@ Devise.setup do |config|
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
   Devise.setup do |config|
-    config.omniauth :facebook, "644481139092321", "277b5e5e551ca21980ea0024fdcdccfe"
-    config.omniauth :vkontakte, "5778288", "GAepQCbdEGBYySpWCfba"
+    #config.omniauth :facebook, "644481139092321", "277b5e5e551ca21980ea0024fdcdccfe"
+    #config.omniauth :vkontakte, "5778288", "GAepQCbdEGBYySpWCfba"
+    config.omniauth :vkontakte, "5797896", "evwCscRJNfMMl8nwEQpq"
+    config.omniauth :facebook, "1399868356724121", "af1d5f188b63d53f788ff93f41ee02d2"
+
   end
 end
