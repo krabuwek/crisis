360fe92020cf7fc176082a7e7cb1a981de9dd72d
Max <krabuwek@outlook.com>
2017-01-10 14:13:23 +0300

    dsadsa

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Rails.application.routes.default_url_options[:host] = 'domain.com'
+Rails.application.routes.default_url_options[:host] = '80.93.177.12'
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
